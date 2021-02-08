44b2e6d6ebf625b5a40502a3ad437b47084aeb8c
daisukesone <sonedai0213@gmail.com>
2021-02-08 09:20:13 +0900

    longest-common-prefix

---
+++ 14.longest-common-prefix.rb
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=14 lang=ruby
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    return "" if strs.empty? 
+    s = ""
+    base = strs[0]
+    for i in 0..base.length-1
+      if strs.all?{|x| x[i] == base[i]}
+        s += base[i]
+      else
+        break
+      end
+    end
+    s
+end
+# @lc code=end
+
